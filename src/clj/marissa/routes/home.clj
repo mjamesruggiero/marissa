8fb7f4ce2c13bf214b64e2834df6534076090c5b
mjamesruggiero <mjamesruggiero@gmail.com>
2017-03-01 18:22:18 -0800

    You can submit values through a form

--- src/clj/marissa/routes/home.clj
+++ src/clj/marissa/routes/home.clj
@@ -1,17 +1,26 @@
 (ns marissa.routes.home
   (:require [marissa.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [marissa.db.core :as db]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+   "home.html"
+   {:messages (db/get-messages)}))
+
+(defn save-message!
+  [{:keys [params]}]
+  (db/save-message!
+   (assoc params :timestamp (java.util.Date.)))
+  (response/found "/"))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
