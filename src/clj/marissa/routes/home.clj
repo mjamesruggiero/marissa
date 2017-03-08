54d584086d99dfbd8ac4c0cb919f8c9a9ee82b91
mjamesruggiero <mjamesruggiero@gmail.com>
2017-03-07 20:38:44 -0800

    Primitive form has validations

--- src/clj/marissa/routes/home.clj
+++ src/clj/marissa/routes/home.clj
@@ -3,24 +3,39 @@
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]
-            [marissa.db.core :as db]))
+            [marissa.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]))
 
-(defn home-page []
+(defn validate-message
+  [params]
+  (first
+   (b/validate
+    params
+    :name v/required
+    :message [v/required [v/min-count 10]])))
+
+(defn home-page [{:keys [flash]}]
   (layout/render
    "home.html"
-   {:messages (db/get-messages)}))
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn save-message!
   [{:keys [params]}]
-  (db/save-message!
-   (assoc params :timestamp (java.util.Date.)))
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+       (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
