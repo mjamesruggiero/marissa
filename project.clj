f2cf188375ed6609385ddc0214bc76e81edb54d7
mjamesruggiero <mjamesruggiero@gmail.com>
2017-03-10 20:30:18 -0800

    Revert "Can wrap Ring handlers"
    This reverts commit 44132ff56f2eb01c10e71ca2f21a02451358631d.

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject marissa "0.1.0-SNAPSHOT"
-  :description "Marissa: experimenting with Ring"
-  :url "http://mjamesruggiero.com"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring "1.5.0"]]
-  :main marissa.core)
