6bb3be42735d989c1ef42e7e357e50d555d2d0a4
mjamesruggiero <mjamesruggiero@gmail.com>
2017-03-12 22:33:21 -0700

    Can render form using Reagent

--- project.clj
+++ project.clj
@@ -28,7 +28,8 @@
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
                  [luminus-immutant "0.1.0"]
                  [luminus-log4j "0.1.2"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [reagent "0.5.1"]]
 
   :min-lein-version "2.0.0"
   :uberjar-name "marissa.jar"
--- src/cljs/marissa/core.cljs
+++ src/cljs/marissa/core.cljs
@@ -1,5 +1,32 @@
-(ns marissa.core)
+(ns marissa.core
+  (:require [reagent.core :as reagent :refer [atom]]))
 
-(-> (.getElementById js/document "content")
-    (.-innerHTML)
-    (set! "Hello World!"))
+(defn home []
+  [:div.row
+   [:div.span12
+    [message-form]]])
+
+(defn message-form []
+  (let [fields (atom {})]
+    (fn []
+      [:div.content
+       [:div.form-group
+        [:p "Name:"
+         [:input.form-control
+          {:type :text
+           :name :name
+           :on-change #(swap! fields assoc :name (-> % .-target .-value))
+           :value (:name @fields)}]]]
+       [:p "Message"
+        [:textarea.form-control
+         {:rows 4
+          :cols 50
+          :name :message
+          :on-change #(swap! fields assoc :message (-> % .-target .-value))}
+         (:message @fields)]]
+       [:input.btn.btn-primary {:type :submit :value "comment"}]])))
+
+(reagent/render
+ [home]
+ (.getElementById js/document "content"))
+
